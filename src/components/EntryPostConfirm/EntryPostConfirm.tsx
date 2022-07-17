6547a35f7552b5f03bcc83f4d0dfdc090e606524
Shota Kubota <ta2@o2p.jp>
2022-07-17 16:53:03 +0900

    feat(EntryPostConfirm): tweak to action buttons

--- src/components/EntryPostConfirm/EntryPostConfirm.tsx
+++ src/components/EntryPostConfirm/EntryPostConfirm.tsx
@@ -6,6 +6,7 @@ import { SubmitButton } from '../SubmitButton';
 import { CancelButton } from '../CancelButton';
 
 import './EntryPostConfirm.css';
+import { useRef } from 'react';
 
 type Props = {
   isOpen: boolean;
@@ -14,14 +15,20 @@ type Props = {
 };
 
 export function EntryPostConfirm({ isOpen, onSubmit, onClose }: Props) {
+  const initialFocusedRef = useRef(null);
+
   return createPortal(
-    <Dialog open={isOpen} onClose={onClose} className="entry-post-confirm">
+    <Dialog initialFocus={initialFocusedRef} open={isOpen} onClose={onClose} className="entry-post-confirm">
       <div className="entry-post-confirm__panel">
         <Dialog.Panel className="entry-post-confirm__contents">
           <Dialog.Title>{retrieveTranslation('confirm.title')}</Dialog.Title>
           <Dialog.Description>{retrieveTranslation('confirm.description')}</Dialog.Description>
-          <SubmitButton onSubmit={onSubmit}>{retrieveTranslation('submit')}</SubmitButton>
-          <CancelButton onSubmit={onClose}>{retrieveTranslation('cancel')}</CancelButton>
+          <div className="entry-post-confirm__actions">
+            <CancelButton ref={initialFocusedRef} onSubmit={onClose}>
+              {retrieveTranslation('cancel')}
+            </CancelButton>
+            <SubmitButton onSubmit={onSubmit}>{retrieveTranslation('submit')}</SubmitButton>
+          </div>
         </Dialog.Panel>
       </div>
     </Dialog>,
