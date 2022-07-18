d9d021abc27ef9f94fef417657c0367204636daf
Shota Kubota <ta2@o2p.jp>
2022-07-18 23:26:46 +0900

    feat(TitleInputForm): stop restore state

--- src/components/TitleInputForm/TitleInputForm.tsx
+++ src/components/TitleInputForm/TitleInputForm.tsx
@@ -1,14 +1,11 @@
 import { FormEvent, useCallback } from 'react';
 
 import { useEntryState } from '../../global_state/entry_state';
-import { useSerializeMetadata } from '../../hooks/use_serialize_metadata';
 import { retrieveTranslation } from '../../locales/i18n';
 
 export function TitleInputForm(): JSX.Element {
   const title = useEntryState((state) => state.title);
   const setTitle = useEntryState((state) => state.setTitle);
-
-  useSerializeMetadata();
 
   const onInputTitle = useCallback((event: FormEvent<HTMLInputElement>) => {
     if (event.target instanceof HTMLInputElement) {
